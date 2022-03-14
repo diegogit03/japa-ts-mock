4c41d52997cbcd40bb79919fb41f44be297a1106
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2022-03-13 21:35:08 -0300

    fix function name

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import type { PluginFn } from '@japa/runner';
 /* MOCKER TYPE */
 export type Mocker = typeof MockitoMocker
 
-export function expect(): PluginFn {
+export function mocker(): PluginFn {
   return function (_, __, { TestContext }) {
     TestContext.getter('mocker', () => MockitoMocker, true)
   }
