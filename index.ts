530eff2d3d4a6c0a320554e030ff7e3c625b1d15
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2022-03-13 21:39:32 -0300

    fixing context name

--- index.ts
+++ index.ts
@@ -2,16 +2,16 @@ import MockitoMocker from 'ts-mockito';
 import type { PluginFn } from '@japa/runner';
 
 /* MOCKER TYPE */
-export type Mocker = typeof MockitoMocker
+export type Mocker = typeof MockitoMocker;
 
 export function mocker(): PluginFn {
   return function (_, __, { TestContext }) {
-    TestContext.getter('mocker', () => MockitoMocker, true)
+    TestContext.getter('mocker', () => MockitoMocker, true);
   }
 }
 
 declare module '@japa/runner' {
   interface TestContext {
-    expect: Mocker
+    mocker: Mocker;
   }
 }
